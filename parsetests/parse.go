9ccc1b0f57032173944ff0ec0208459ac7d68a37
Emily Marx <emilykmarx@alumni.stanford.edu>
2026-07-02 18:59:53 -0700

    Group param influence info by {msg type, CType} (since param keys are the same
    for all CTypes, and we don't currently do data-flow checks).
    
    Also dump to csv - format:
    API,Verb,Resource,CType,Param keys
    
    Would probably be more useful for csv to just have msg info and CTypes, and CType => key info
    to be in a separate file arranged more similarly to the config file.

--- parsetests/parse.go
+++ parsetests/parse.go
@@ -1,41 +1,57 @@
 package parsetests
 
 import (
+	"encoding/csv"
+	"os"
+
 	"github.com/emilykmarx/conftamer/pkg/apimessages"
-	"github.com/emilykmarx/conftamer/pkg/ctypes"
 )
 
 /* Functions for recording info during tests. */
+type ParamKeys []string
 
-// A CType method and corresponding params
-type MethodParams struct {
-	method string
-	params []ctypes.CTypeParam
-}
 type APIMessageInfo struct {
-	controlFlow map[string]map[string]struct{} // param key => methods that found CF from param to msg
+	controlFlow map[string]ParamKeys // receiver => param keys it accesses
 }
 
 // Info for each msg gathered across all tests (API call ID => influence)
 type AllTaint map[apimessages.APICallID]APIMessageInfo
 
-func (m *AllTaint) AddCTypeMethodCall(api_call_id apimessages.APICallID, param_keys []string, method string) {
+func (m *AllTaint) Dump(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	rows :=
+		[][]string{{
+			"API", "Verb", "Resource", "CType", "Param key",
+		}}
+
+	for api_call_ID, msg_info := range *m {
+		for recvr_type, keys := range msg_info.controlFlow {
+			// Row for each recvr that sends this msg
+			row := []string{api_call_ID.API, api_call_ID.Verb, api_call_ID.Resource}
+			row = append(row, recvr_type)
+			row = append(row, keys...)
+			rows = append(rows, row)
+		}
+	}
+
+	return w.WriteAll(rows)
+}
+
+func (m *AllTaint) AddCTypeMethodCall(api_call_id apimessages.APICallID, param_keys []string, recvr_type string) {
 	// Can't edit map value in place => get it (initializing its maps if needed) and put it back
 
-	// XXX write to csv (split out msg ID parts)
 	existing_flow := (*m)[api_call_id]
 	if existing_flow.controlFlow == nil {
-		existing_flow.controlFlow = make(map[string]map[string]struct{})
+		existing_flow.controlFlow = make(map[string]ParamKeys)
 	}
-
-	for _, param_key := range param_keys {
-		existing_methods := existing_flow.controlFlow[param_key]
-		if existing_methods == nil {
-			existing_methods = make(map[string]struct{})
-		}
-		existing_methods[method] = struct{}{}
-		existing_flow.controlFlow[param_key] = existing_methods
-	}
+	existing_flow.controlFlow[recvr_type] = param_keys
 
 	(*m)[api_call_id] = existing_flow
 }
